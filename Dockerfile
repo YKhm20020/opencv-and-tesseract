38d02466a6ae1a811e64de84a4944c1c23df0d4d
ykimura0726 <y.kimura@dopponica.jp>
2023-05-09 19:40:55 +0900

    テスト

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM ubuntu:latest
+
+ENV PYTHON_VERSION 3.7.1
+ENV HOME /root
+ENV PYTHON_ROOT $HOME/local/python-$PYTHON_VERSION
+ENV PATH $PYTHON_ROOT/bin:$PATH
+ENV PYENV_ROOT $HOME/.pyenv
+
+# タイムゾーン
+RUN ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+
+# apt
+RUN apt update
+RUN apt install -y libopencv-dev
+
+RUN apt-get update -y \
+    && apt-get upgrade -y \
+    && apt-get autoremove -y \
+    && apt-get install -y software-properties-common \
+    && add-apt-repository ppa:git-core/ppa \
+    && apt-get update -y
+
+RUN apt-get install -y git 
+
+# install python and pip
+RUN apt install -y python3 python3-pip
+RUN pip install --upgrade pip
+
+# set working directory and copy files
+WORKDIR /usr/src/app
+COPY ./ /usr/src/app
+
+# install opencv
+RUN pip install opencv-python
---
+++ export_array.py
@@ -0,0 +1,48 @@
+import cv2
+from IPython.display import Image, display
+import numpy as np
+import copy
+import json
+
+#img.shapeに高さ、幅、チャンネル情報を分割代入
+img = cv2.imread('./sample.png')
+height, width, channels = img.shape
+
+#インプット画像をグレースケール化
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Cannyを用いたエッジ検出
+threshold1 = 800
+threshold2 = 800
+edges = cv2.Canny(img_gray, threshold1, threshold2)
+
+#膨張処理（線を太くする）
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+dilates = cv2.dilate(edges, kernel)
+
+#輪郭検出
+contours, hierarchy = cv2.findContours(dilates, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# エクスポート用イメージ配列を作成しておく
+export_img = copy.deepcopy(img)
+
+# JSON用の辞書型配列
+export_array = {
+    'width': width,
+    'height': height,
+    'results': []
+}
+
+for i in range(len(contours)):
+    # 色を指定する
+    color = np.random.randint(0, 255, 3).tolist()
+    
+    if cv2.contourArea(contours[i]) < 3000:
+        continue  # 面積が小さいものは除く
+    
+    # 階層が第１じゃなかったら ... 
+    if hierarchy[0][i]<img class="ranking-number" src="https://proglearn.com/wp-content/themes/jin/img/rank03.png" /> != -1:
+        # 配列に追加
+        export_array['results'].append(create_pos_data(contours[i]))
+        # 画像に当該の枠線を追加
+        cv2.drawContours(export_img, contours, i, color, 3)  
