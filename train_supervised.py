9d538eb861269e9827ca693cda3d340709a67a79
ykimura0726 <y.kimura@dopponica.jp>
2023-08-13 20:03:05 +0900

    add: 本日進捗分を更新

---
+++ train_supervised.py
@@ -0,0 +1,19 @@
+import fasttext
+
+model = fasttext.train_supervised(input = 'train_fasttext.txt')
+model.save_model('photolize.bin')
+
+with open('train_fasttext.txt', 'r') as f:
+  text = f.read()
+  words_date = text.count('date')
+  words_num = text.count('num')
+  words_char = text.count('char')
+  words_boolean = text.count('boolean')
+
+print(f'\ndate: {words_date}')
+print(f'num: {words_num}')
+print(f'char: {words_char}')
+print(f'boolean: {words_boolean}')
+
+ret = model.predict("年齢")
+print(ret)
